8f91703cbf388ee95e9d34762d720150a484381b
Léo Eichhorn <leo.eichhorn@etu.unistra.fr>
2022-05-11 15:00:30 +0200

    Made the main using the new structure + added the stress test of the websocket function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
 import time
 
 import pymongo
+
+import DataPretierAndConverter
 from WebsocketManager import WebsocketManager
 from Logger import Logger
 from DBInjector import DBInjector
@@ -26,7 +29,7 @@ except json.JSONDecodeError as jsonDecodeError:
 
 DEBUG: bool = conf["DEBUG"]
 DEVICE_ID: str = conf["device_id"]
-DEVICE_NAME: List[str] = conf["device_name"]
+DEVICE_LABEL: List[str] = conf["device_label"]
 logger = Logger("./log.txt", DEBUG)
 
 logger.info_print("script started successfully")
@@ -43,27 +46,55 @@ except pymongo.mongo_client.ServerSelectionTimeoutError as sste:
 # set the database name to DATA
 database = client["DATA"]
 
+if DEBUG:
+    client.drop_database(database)
+
 # create a collection of raw_data
 device_collection = database["DEVICES"]
 if "device_id_1" not in device_collection.index_information():
     device_collection.create_index("device_id", unique=True)
-device = device_collection.find_one({"device_id": DEVICE_ID})
+
 sensor_collection = database["SENSORS"]
 if "sensor_id_1" not in sensor_collection.index_information():
-    sensor_collection.create_index("sensor_id", unique=True)
+    sensor_collection.create_index("sensor", unique=True)
+    sensor_collection.create_index("sensor_code", unique=True)
+
+counter_collection = database["COUNTER"]
+if counter_collection.count_documents({}) == 0:
+    counter_collection.insert_one({"status_count": 0})
+    counter_collection.insert_one({"sensor_count": 0})
+
+status_collection = database["STATUS"]
+if "status_1" not in status_collection.index_information():
+    status_collection.create_index("status", unique=True)
+
+DataPretierAndConverter.counter_collection = counter_collection
+DataPretierAndConverter.sensor_collection = sensor_collection
+DataPretierAndConverter.status_collection = status_collection
+
+device = device_collection.find_one({"device_id": DEVICE_ID})
 logger.debug_print(device)
+
 if not device:
-    device_collection.insert_one({"device_id": DEVICE_ID, "device_name": conf["device_name"]})
+    device_collection.insert_one({"device_id": DEVICE_ID, "device_label": DEVICE_LABEL})
     logger.debug_print("Created new device entry in \"DEVICES\"")
-elif device["device_name"] != DEVICE_NAME:
-    device_collection.update_one({"device_id": DEVICE_ID}, {"$set": {"device_name": DEVICE_NAME}})
+elif device["device_label"] != DEVICE_LABEL:
+    device_collection.update_one({"device_id": DEVICE_ID}, {"$set": {"device_label": DEVICE_LABEL}})
     logger.debug_print("Updating names of the device")
+
+DEVICE_ID = device_collection.find_one({"device_id": DEVICE_ID})["_id"]
 
 websocketManager = WebsocketManager(database, DEVICE_ID, logger)
 
 # TODO read the real websocket here
 
-websocketManager.on_incoming_message('{"timestamp": "2022-05-10 11:06:03+02:00", "RFID_status": "ON", "IR_status": {}, "Sensor": "", "Status": "Waiting a tag to read", "Alarm": "False", "bat_percent": 0, "bat_voltage": 0, "temperature_board": 0, "type": "information 279"}')
+logger.info_print("Starting stress test of websocket")
+startTime = time.time() * 1000
+for i in range(10000):
+    websocketManager.on_incoming_message('{"timestamp": "2022-05-10 11:06:03+02:00", "RFID_status": "ON", "IR_status": {}, "Sensor": "", "Status": "Waiting a tag to read", "Alarm": "False", "bat_percent": 0, "bat_voltage": 0, "temperature_board": 0, "type": "information 279"}')
+endTime = time.time() * 1000
+logger.info_print("Ended test of websocket stress test : took " + str(int(endTime - startTime)) + "ms")
+
 
 logger.info_print("Starting test of db injector")
 startTime = time.time() * 1000
@@ -72,20 +103,8 @@ dbInjector.inject_file_in_db()
 endTime = time.time() * 1000
 logger.info_print("Ended test of db injector : took " + str(int(endTime - startTime)) + "ms")
 
-for data in device_collection.find():
-    logger.debug_print(data)
-    logger.debug_print(data["device_id"].upper())
-    for other_data in database["DEVICE_INFO_"+data["device_id"].upper()].find():
-        logger.debug_print(other_data)
-
-for data in sensor_collection.find():
-    logger.debug_print(data)
-    logger.debug_print(data["sensor_id"])
-    for other_data in database["SENSOR_"+data["sensor_id"]].find():
-        logger.debug_print(other_data)
-
-if DEBUG:
-    client.drop_database(database)
+for status in status_collection.find():
+    print(status)
 
 logger.info_print("script ended successfully")
 
