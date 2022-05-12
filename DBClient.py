e7e6c46308cea8b7160ae6f5b01a97614917b6cc
Léo Eichhorn <leo.eichhorn@etu.unistra.fr>
2022-05-12 10:33:28 +0200

    Made a class that will create or get the used database and applied it to the main

---
+++ DBClient.py
@@ -0,0 +1,69 @@
+from typing import List
+
+import DataPretierAndConverter
+from DBManager import DBManager
+from Logger import Logger
+from pymongo.collection import ObjectId
+
+
+class DBClient:
+    def __init__(self, db_manager: DBManager, logger: Logger = None) -> None:
+        """
+        Constructor that will create or get main database and create the index of them
+        :param db_manager: The db_manager that will be used
+        :param logger: The logger that will be used
+        """
+        self.db_manager = db_manager
+        self.logger = logger
+
+        self.database = self.db_manager.get_database("DATA")
+
+        # create the collections
+        self.device_collection = self.database["DEVICES"]
+        if "device_id_1" not in self.device_collection.index_information():
+            self.device_collection.create_index("device_id", unique=True)
+
+        self.sensor_collection = self.database["SENSORS"]
+        if "sensor_code_1" not in self.sensor_collection.index_information():
+            self.sensor_collection.create_index("sensor_code", unique=True)
+
+        self.counter_collection = self.database["COUNTER"]
+        if self.counter_collection.count_documents({}) == 0:
+            self.counter_collection.insert_one({"status_count": 0})
+            self.counter_collection.insert_one({"sensor_count": 0})
+
+        self.status_collection = self.database["STATUS"]
+
+        DataPretierAndConverter.counter_collection = self.counter_collection
+        DataPretierAndConverter.sensor_collection = self.sensor_collection
+        DataPretierAndConverter.status_collection = self.status_collection
+
+    def get_or_update_device(self, device_id: str, device_label: List[str]) -> ObjectId:
+        """
+        Function that will get or update the device label, and it will get the device id
+        :param device_id: The device_id to get or update
+        :param device_label: The list of label that can have the device
+        :return: The _id of the device (take less storage)
+        """
+        device = self.device_collection.find_one({"device_id": device_id})
+        if self.logger:
+            self.logger.debug_print(device)
+
+        if not device:
+            self.device_collection.insert_one({"device_id": device_id, "device_label": device_label})
+            if self.logger:
+                self.logger.debug_print("Created new device entry in \"DEVICES\"")
+        elif device["device_label"] != device_label:
+            self.device_collection.update_one({"device_id": device_id}, {"$set": {"device_label": device_label}})
+            if self.logger:
+                self.logger.debug_print("Updating names of the device")
+
+        return self.device_collection.find_one({"device_id": device_id})["_id"]
+
+    def close_connection(self):
+        """
+        Function that will close the connection to the mongoDB server
+        """
+        self.db_manager.close_connection()
+
+
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time
-import DataPretierAndConverter
 import Util
 from WebsocketManager import WebsocketManager
 from Logger import Logger
 from DBInjector import DBInjector
 from typing import List
 from DBManager import DBManager
+from DBClient import DBClient
 
 conf = Util.load_config()
 
@@ -22,41 +22,10 @@ db_manager = DBManager(address=conf["server"]["address"], port=conf["server"]["p
 if DEBUG:
     db_manager.drop_database("DATA")
 
-database = db_manager.get_database("DATA")
+db_client = DBClient(db_manager)
+db_client.get_or_update_device(DEVICE_ID, DEVICE_LABEL)
 
-# create the collections
-device_collection = database["DEVICES"]
-if "device_id_1" not in device_collection.index_information():
-    device_collection.create_index("device_id", unique=True)
-
-sensor_collection = database["SENSORS"]
-if "sensor_code_1" not in sensor_collection.index_information():
-    sensor_collection.create_index("sensor_code", unique=True)
-
-counter_collection = database["COUNTER"]
-if counter_collection.count_documents({}) == 0:
-    counter_collection.insert_one({"status_count": 0})
-    counter_collection.insert_one({"sensor_count": 0})
-
-status_collection = database["STATUS"]
-
-DataPretierAndConverter.counter_collection = counter_collection
-DataPretierAndConverter.sensor_collection = sensor_collection
-DataPretierAndConverter.status_collection = status_collection
-
-device = device_collection.find_one({"device_id": DEVICE_ID})
-logger.debug_print(device)
-
-if not device:
-    device_collection.insert_one({"device_id": DEVICE_ID, "device_label": DEVICE_LABEL})
-    logger.debug_print("Created new device entry in \"DEVICES\"")
-elif device["device_label"] != DEVICE_LABEL:
-    device_collection.update_one({"device_id": DEVICE_ID}, {"$set": {"device_label": DEVICE_LABEL}})
-    logger.debug_print("Updating names of the device")
-
-DEVICE_ID = device_collection.find_one({"device_id": DEVICE_ID})["_id"]
-
-websocketManager = WebsocketManager(database, DEVICE_ID, logger)
+websocketManager = WebsocketManager(db_client.database, DEVICE_ID, logger)
 
 # TODO read the real websocket here
 
@@ -82,16 +51,17 @@ logger.info_print("Ended test of websocket stress test : took " + str(int(endTim
 
 logger.info_print("Starting test of db injector")
 startTime = time.time() * 1000
-dbInjector = DBInjector(database, "./BirdDevice18_220327.csv", DEVICE_ID, logger)
+dbInjector = DBInjector(db_client.database, "./BirdDevice18_220327.csv", DEVICE_ID, logger)
 dbInjector.inject_file_in_db()
 endTime = time.time() * 1000
+
 logger.info_print("Ended test of db injector : took " + str(int(endTime - startTime)) + "ms")
 
-for status in status_collection.find():
+for status in db_client.status_collection.find():
     logger.debug_print(status)
 
 # Test of aggregation
-result = database["DATA"].aggregate([
+result = db_client.database["DATA"].aggregate([
     {
         "$lookup": {
             "from": "DEVICES",
@@ -127,4 +97,4 @@ for res in result:
 
 logger.info_print("script ended successfully")
 
-db_manager.close_connection()
+db_client.close_connection()
